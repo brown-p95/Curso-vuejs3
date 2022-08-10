a1a6af349f58229eb8c25d2f11476693de316264
Pablo <jpgm.nomercy@gmail.com>
2022-08-10 14:58:18 -0500

    Hola mundo con vue.js y banco

--- Hola Mundo/main.js
+++ Hola Mundo/main.js
@@ -1,1 +1,9 @@
-console.log('funcionando')
+const app = Vue.createApp({
+    data(){
+        return {
+            titulo:'Este es un Hola mundo desde vue.js',
+            cantidad: 999.9,
+            enlace: 'https://www.youtube.com/'
+        }
+    }
+})
