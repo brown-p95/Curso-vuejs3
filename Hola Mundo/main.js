6c37265f9c9f7590ff5a991849dd6c9ae32b07e0
Pablo <jpgm.nomercy@gmail.com>
2022-08-10 16:10:13 -0500

    day2

--- Hola Mundo/main.js
+++ Hola Mundo/main.js
@@ -2,8 +2,25 @@ const app = Vue.createApp({
     data(){
         return {
             titulo:'Este es un Hola mundo desde vue.js',
-            cantidad: 999.9,
-            enlace: 'https://www.youtube.com/'
+            cantidad: 10,
+            enlace: 'https://www.youtube.com/',
+            estado: false,
+            servicios: ['pagos','retiros','tranferencia','consulta']
+          
         }
+    },
+    methods: {
+        quitar100(){
+            if(this.cantidad>100)
+            this.cantidad-=100;
+            else
+            alert('no se puede quitar mas')
+        },
+        agregar100(){
+            
+            this.cantidad+=100;
+        }
+        
+
     }
 })
