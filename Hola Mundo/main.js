dcfd98a59a24f8a912b9f520e9a0f21a8f34dd24
Pablo <jpgm.nomercy@gmail.com>
2022-08-11 10:32:49 -0500

    desactivar botones

--- Hola Mundo/main.js
+++ Hola Mundo/main.js
@@ -2,10 +2,17 @@ const app = Vue.createApp({
     data(){
         return {
             titulo:'Este es un Hola mundo desde vue.js',
-            cantidad: 10,
+            cantidad: 100,
             enlace: 'https://www.youtube.com/',
             estado: false,
-            servicios: ['pagos','retiros','tranferencia','consulta']
+            servicios: ['pagos','retiros','tranferencia','consulta'],
+            statusbotonquitar: false,
+            get estadoboton() {
+                return this.statusbotonquitar;
+            },
+            set estadoboton(value) {
+                this.statusbotonquitar = value;
+            },
           
         }
     },
@@ -17,9 +24,26 @@ const app = Vue.createApp({
             alert('no se puede quitar mas')
         },
         agregar100(){
-            
+            this.estadoboton=false;
             this.cantidad+=100;
-        }
+        },
+        quitar10(){
+            if(this.cantidad>10)
+            this.cantidad-=10;
+            else if(this.cantidad==10){
+            this.cantidad-=10;
+            alert('La Cuenta quedara en 0, no se puede descontar mas')
+            this.estadoboton=true;
+            return
+            }
+            else
+            alert('Cuenta en 0, no se puede descontar mas')
+        },
+        agregar10(){
+            this.cantidad+=10;
+            this.estadoboton=false;
+        },
+        
         
 
     }
